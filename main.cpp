f56c8ce5aa2516cbecb770890fe0a04cf326837f
izawartka <59137928+izawartka@users.noreply.github.com>
2025-04-26 01:47:35 +0200

    Race UI placement fix
    +Added fixes for some UI elements that should be aligned to the right
       +Most of graphics shown during the race
       +All text shown during the race
       +RPM, speed and fuel level gauges
    +Added an option to use CALL instead of JMP FAR in BOOL PrimitiveHookFunction
    +Added BOOL HookMultipleCallBasedFunctions
    *Cleaned up the code (not enough lol)
    -Removed some weird temp file

--- main.cpp
+++ main.cpp
@@ -5,13 +5,54 @@
 #define DEFAULT_SCREEN_HEIGHT 720
 #define DEFAULT_SCREEN_BITDEPTH 32
 
+// stretched - (0 = left side of the screen, 1000 = right side of the screen)
+// left aligned (original) - (0 = left side of the screen, 1000 != right side of the screen)
+// right aligned - (0 != left side of the screen, 1000 = right side of the screen)
+#define SCALED_X(x) (screenUIWidthScale * (x)) // stretched scaling
+#define SCALED_Y(y) (screenUIScale * (y)) // left aligned (original) scaling
+#define SCALED_V(x) (SCALED_X(1000) - SCALED_Y(1000 - x)) // right aligned scaling
+#define SCALED_RECTS(x1, y1, x2, y2) SCALED_X(x1), SCALED_Y(y1), SCALED_X(x2), SCALED_Y(y2) // stretched rect scaling
+#define SCALED_RECTO(x1, y1, x2, y2) SCALED_Y(x1), SCALED_Y(y1), SCALED_Y(x2), SCALED_Y(y2) // left aligned (original) rect scaling
+#define SCALED_RECTV(x1, y1, x2, y2) SCALED_V(x1), SCALED_Y(y1), SCALED_V(x2), SCALED_Y(y2) // right aligned rect scaling
+
 static DWORD pScreenWidth = 0x006d196c;
 static DWORD pScreenHeight = 0x006d1968;
 static DWORD pScreenBitdepth = 0x006d1970;
 static DWORD pScreenUIScale = 0x006d1974;
-static DWORD pStartScreenOptionsList = 0x00401e9C;
-static DWORD pAfterScreenOptionsList = 0x00401ee6;
-static DWORD pDirect3DInitFn = 0x004400a2;
+
+static DWORD pScreenOptionsListJump = 0x00401e9C;
+static DWORD pScreenOptionsListReturn = 0x00401ee6;
+
+static DWORD pFixUIPlacementJump = 0x0040c888;
+static DWORD pFixUISomeStruct = 0x006d1af8;
+static DWORD pFixUIPlacementReturn = 0x0040cc0f;
+
+static DWORD pFixGaugesPlacementSomeFn = 0x0044b690;
+static DWORD pFixGaugesPlacementCalls[] = {
+	0x0040aa65, // RPM
+	0x0040a823, // Speed
+	0x0040ba89, // Fuel
+};
+
+static DWORD pFixRaceTextPlacementSomeFn = 0x0044e070;
+static DWORD pFixRaceTextPlacementCalls[] = {
+	0x0040aba7,
+	0x0040abe0,
+	0x004a6eaf,
+	0x004a6eea,
+	0x004a6f9c,
+	0x004a6fd7,
+	0x004a70cb,
+	0x004a7106,
+	0x004a71b6,
+	0x004a71f1,
+	0x004a7266,
+	0x004a72a1
+};
+
+//void __thiscall drawTexture(int param_1_00,char *path,float x1,float y1,float x2,float y2,int flags)
+typedef void(__thiscall* DrawTexture)(int param_1_00, char* path, float x1, float y1, float x2, float y2, int flags);
+static DrawTexture fnDrawTexture = (DrawTexture)0x0044c3d0;
 
 const char* customScreenOptionsText = "Custom [Hook]";
 
@@ -24,29 +65,8 @@ int screenWidth = DEFAULT_SCREEN_WIDTH;
 int screenHeight = DEFAULT_SCREEN_HEIGHT;
 int screenBitdepth = DEFAULT_SCREEN_BITDEPTH;
 float screenUIScale;
-
-static __declspec(naked) void Direct3DInitHook(void) {
-    __asm {
-        NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-    }
-
-    // MessageBox(NULL, "Hello", "Seuko", 0);
-	*(int*)pScreenWidth = screenWidth;
-	*(int*)pScreenHeight = screenHeight;
-	*(int*)pScreenBitdepth = screenBitdepth;
-	*(float*)pScreenUIScale = screenUIScale;
-
-	__asm {
-		MOV EAX, pDirect3DInitFn
-		add eax, 5
-		JMP EAX
-	}
-}
+float screenUIWidthScale;
+float rightSideShift;
 
 static __declspec(naked) void SkipScreenOptionsListHook(void) {
 	// Screen options' hWnd is stored in ECX
@@ -71,11 +91,67 @@ static __declspec(naked) void SkipScreenOptionsListHook(void) {
 		call SendMessageA
 
 		// Go back to the program
-		JMP pAfterScreenOptionsList
+		JMP pScreenOptionsListReturn
 	}
 }
 
-BOOL PrimitiveHookFunction(const DWORD originalFn, DWORD hookFn, size_t copyBytes) {
+static __declspec(naked) void FixUIPlacementHook(void) {
+	fnDrawTexture(pFixUISomeStruct + 0x640, "hud_ico_czas.tga", SCALED_RECTV(820, 2, 1000, 92), -1);
+	fnDrawTexture(pFixUISomeStruct + 0x6ec, "hud_ico_miejsce.tga", SCALED_RECTV(820, 56, 1000, 146), -1);
+	fnDrawTexture(pFixUISomeStruct + 0x798, "hud_ico_lap.tga", SCALED_RECTV(820, 130, 1000, 220), -1);
+	fnDrawTexture(pFixUISomeStruct + 0x000, "hud_car_ja.tga", SCALED_RECTO(98, 654, 114, 670), -1);
+	fnDrawTexture(pFixUISomeStruct + 0x0ac, "hud_car_wrog.tga", SCALED_RECTO(98, 654, 114, 670), -1);
+	fnDrawTexture(pFixUISomeStruct + 0x158, "hud_car_policja.tga", SCALED_RECTO(98, 654, 114, 670), -1);
+	fnDrawTexture(pFixUISomeStruct + 0x43c, "hud_ico_N2O_niegotowy.tga", SCALED_RECTV(720, 2, 784, 66), -1);
+	fnDrawTexture(pFixUISomeStruct + 0x4e8, "hud_ico_N2O_gotowy.tga", SCALED_RECTV(720, 2, 784, 66), -1);
+	fnDrawTexture(pFixUISomeStruct + 0x2b8, "hud_ico_radar_czerwony.tga", SCALED_RECTV(640, 2, 704, 66), -1);
+	fnDrawTexture(pFixUISomeStruct + 0x364, "hud_ico_radar_zielony.tga", SCALED_RECTV(640, 2, 704, 66), -1);
+	fnDrawTexture(pFixUISomeStruct + 0x204, "hud_zakaz_r.tga", SCALED_RECTV(560, 2, 624, 66), -1);
+	fnDrawTexture(pFixUISomeStruct + 0xbb0, "hud_paliwo.tga", SCALED_RECTV(896, 596, 1024, 724), -1);
+	fnDrawTexture(pFixUISomeStruct + 0xc5c, "hud_paliwo_wskazowka.tga", 0.0f, 0.0f, 256.0f, 256.0f, -1);
+	fnDrawTexture(pFixUISomeStruct + 0x9ac, "hud_predkosciomierz.tga", SCALED_RECTV(738, 532, 994, 788), -1);
+	fnDrawTexture(pFixUISomeStruct + 0xb04, "hud_wskazowka_predkosc.tga", 0.0f, 0.0f, 256.0f, 256.0f, -1);
+	fnDrawTexture(pFixUISomeStruct + 0xa58, "hud_wskazowka_obroty.tga", 1000.0f, 0.0f, 256.0f, 256.0f, -1);
+	/// TODO: rest of the function
+
+	__asm {
+		JMP pFixUIPlacementReturn
+	}
+}
+
+static __declspec(naked) void FixGaugesPlacementHook(void) {
+	__asm {
+		MOVSS xmm0, rightSideShift
+		ADDSS xmm0, [esp + 4]
+		MOVSS[esp + 4], xmm0
+
+		MOVSS xmm0, rightSideShift
+		ADDSS xmm0, [esp + 12]
+		MOVSS [esp + 12], xmm0
+
+		MOVSS xmm0, rightSideShift
+		ADDSS xmm0, [esp + 20]
+		MOVSS [esp + 20], xmm0
+
+		MOVSS xmm0, rightSideShift
+		ADDSS xmm0, [esp + 28]
+		MOVSS[esp + 28], xmm0
+
+		JMP pFixGaugesPlacementSomeFn
+	}
+}
+
+static __declspec(naked) void FixRaceTextPlacementHook(void) {
+	__asm {
+		MOVSS xmm0, rightSideShift
+		ADDSS xmm0, [esp + 8]
+		MOVSS[esp + 8], xmm0
+
+		JMP pFixRaceTextPlacementSomeFn
+	}
+}
+
+BOOL PrimitiveHookFunction(const DWORD originalFn, DWORD hookFn, size_t copyBytes, bool callBased = false) {
 	DWORD OldProtection = { 0 };
 	BOOL success = VirtualProtectEx(GetCurrentProcess(), (LPVOID)originalFn, copyBytes, PAGE_EXECUTE_READWRITE, &OldProtection);
 	if (!success) {
@@ -83,8 +159,8 @@ BOOL PrimitiveHookFunction(const DWORD originalFn, DWORD hookFn, size_t copyByte
 		return 0;
 	}
 
-	*(BYTE*)((LPBYTE)originalFn) = 0xE9; //JMP FAR
-	DWORD offset = (((DWORD)hookFn) - ((DWORD)originalFn + 5)); //Offset math.
+	*(BYTE*)((LPBYTE)originalFn) = callBased ? 0xE8 : 0xE9; // CALL / JMP FAR
+	DWORD offset = (((DWORD)hookFn) - ((DWORD)originalFn + 5)); // Offset math
 	*(DWORD*)((LPBYTE)originalFn + 1) = offset;
 
 	for (size_t i = 5; i < copyBytes; i++) {
@@ -92,6 +168,17 @@ BOOL PrimitiveHookFunction(const DWORD originalFn, DWORD hookFn, size_t copyByte
 	}
 
 	return 1;
+}
+
+BOOL HookMultipleCallBasedFunctions(const DWORD originalFns[], size_t fnCount, DWORD hookFn) {
+	bool allSuccess = true;
+
+	for (size_t i = 0; i < fnCount; i++) {
+		if (!PrimitiveHookFunction(originalFns[i], hookFn, 5, true)) {
+			allSuccess = false;
+		}
+	}
+	return allSuccess;
 }
 
 BOOL HookFunction(const DWORD originalFn, DWORD hookFn, size_t copyBytes) {
@@ -109,6 +196,7 @@ BOOL HookFunction(const DWORD originalFn, DWORD hookFn, size_t copyBytes) {
 	return PrimitiveHookFunction(originalFn, hookFn, copyBytes);
 }
 
+// Replaces given number of bytes with NOPs (0x90) at the given address
 BOOL ReplaceWithNoOp(const DWORD address, size_t bytes) {
 	DWORD OldProtection = { 0 };
 	BOOL success = VirtualProtectEx(GetCurrentProcess(), (LPVOID)address, bytes, PAGE_EXECUTE_READWRITE, &OldProtection);
@@ -128,6 +216,8 @@ void createHooks() {
 	Config::ReadOrDefaultFromConfig(SECTION, HEIGHT_KEY, screenHeight, DEFAULT_SCREEN_HEIGHT);
 	Config::ReadOrDefaultFromConfig(SECTION, BITDEPTH_KEY, screenBitdepth, DEFAULT_SCREEN_BITDEPTH);
 	screenUIScale = screenHeight / 3.0f * 4.0f * 0.0009765625f;
+	screenUIWidthScale = screenWidth * 0.0009765625f;
+	rightSideShift = (SCALED_X(994) - SCALED_Y(994));
 
 	// disable setting screen options (width, height, bitdepth) & ui scale by the game
 	ReplaceWithNoOp(0x004021f3, 6);	 // sets screen height from the start menu
@@ -139,10 +229,14 @@ void createHooks() {
 	ReplaceWithNoOp(0x0043cc52, 5);  // loads screen bitdepth from config
 	ReplaceWithNoOp(0x0043da62, 6);  // calculates ui scale from config
 
-	PrimitiveHookFunction(pStartScreenOptionsList, (DWORD)SkipScreenOptionsListHook, 5);
-	//HookFunction(pDirect3DInitFn, (DWORD)Direct3DInitHook, 5);
+	PrimitiveHookFunction(pScreenOptionsListJump, (DWORD)SkipScreenOptionsListHook, 5);
 
-	// inject values to the game
+	// fix ui elements placement
+	PrimitiveHookFunction(pFixUIPlacementJump, (DWORD)FixUIPlacementHook, 5);
+	HookMultipleCallBasedFunctions(pFixGaugesPlacementCalls, sizeof(pFixGaugesPlacementCalls) / sizeof(DWORD), (DWORD)FixGaugesPlacementHook);
+	HookMultipleCallBasedFunctions(pFixRaceTextPlacementCalls, sizeof(pFixRaceTextPlacementCalls) / sizeof(DWORD), (DWORD)FixRaceTextPlacementHook);
+
+	// write new screen options to the game
 	*(int*)pScreenWidth = screenWidth;
 	*(int*)pScreenHeight = screenHeight;
 	*(int*)pScreenBitdepth = screenBitdepth;
